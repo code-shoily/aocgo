eb4e4e668ad9e7d5936221be078f0ad2f5bf6811
Mafinar K <mafinar@gmail.com>
2023-01-12 22:08:10 -0500

    Add 19/2 to Runner

--- year19/runner.go
+++ year19/runner.go
@@ -3,12 +3,15 @@ package year19
 import (
 	"fmt"
 	"github.com/code-shoily/aocgo/year19/day01"
+	"github.com/code-shoily/aocgo/year19/day02"
 )
 
 func SolveForDay(day int) {
 	switch day {
 	case 1:
 		day01.Run()
+	case 2:
+		day02.Run()
 	default:
 		fmt.Printf("2019/%d has not been solved yet", day)
 	}
