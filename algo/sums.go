1b72713d23979570d91dec48e3f4e9c8569ac4ba
Mafinar K <mafinar@gmail.com>
2023-01-23 21:12:12 -0500

    Update sum add subarray sum

--- algo/sums.go
+++ algo/sums.go
@@ -1,7 +1,12 @@
 package algo
 
-// TwoSum returns the two values of an int slice that sums to target.
-func TwoSum(sortedData []int, target int) (int, int, bool) {
+import (
+	"golang.org/x/exp/constraints"
+	"sort"
+)
+
+// TwoSumSorted returns the two values of a sorted number slice that sums to target.
+func TwoSumSorted[N constraints.Integer](sortedData []N, target N) (N, N, bool) {
 	left := 0
 	right := len(sortedData) - 1
 
@@ -17,19 +22,63 @@ func TwoSum(sortedData []int, target int) (int, int, bool) {
 		}
 	}
 
-	return -1, -1, false
+	return 0, 0, false
 }
 
-// ThreeSum returns three values of a slice that sums to target.
-func ThreeSum(sortedData []int, target int) (int, int, int, bool) {
+// ThreeSumSorted returns three values of a sorted number slice that sums to target.
+func ThreeSumSorted[N constraints.Integer](sortedData []N, target N) (N, N, N, bool) {
 	for current := 0; current < len(sortedData); current++ {
 		currentValue := sortedData[current]
 		remaining := target - currentValue
 
-		if a, b, ok := TwoSum(sortedData[current+1:], remaining); ok {
+		if a, b, ok := TwoSumSorted[N](sortedData[current+1:], remaining); ok {
 			return currentValue, a, b, true
 		}
 	}
 
-	return -1, -1, -1, false
+	return 0, 0, 0, false
+}
+
+// TwoSum returns the two values of a number slice that sums to target.
+func TwoSum[N constraints.Integer](data []N, target N) (N, N, bool) {
+	sortedData := make([]N, len(data))
+	copy(sortedData, data)
+	sort.Slice(sortedData, func(i, j int) bool {
+		return sortedData[i] < sortedData[j]
+	})
+
+	return TwoSumSorted(sortedData, target)
+}
+
+// ThreeSum returns three values of a number slice that sums to target.
+func ThreeSum[N constraints.Integer](data []N, target N) (N, N, N, bool) {
+	sortedData := make([]N, len(data))
+	copy(sortedData, data)
+	sort.Slice(sortedData, func(i, j int) bool {
+		return sortedData[i] < sortedData[j]
+	})
+
+	return ThreeSumSorted(sortedData, target)
+}
+
+// SubArraySum returns the subarray that sums to given target
+func SubArraySum[N constraints.Integer](data []N, target N) (from, to int, found bool) {
+	// FIXME: What if there are duplicates?
+	memo := map[N]int{}
+	var runningSum N
+	for currentIdx, v := range data {
+		runningSum += v
+
+		if runningSum == target {
+			return 0, currentIdx, true
+		}
+
+		if start, exists := memo[runningSum-target]; exists {
+			return start + 1, currentIdx, true
+		}
+
+		memo[runningSum] = currentIdx
+	}
+
+	return
 }
--- algo/sums_test.go
+++ algo/sums_test.go
@@ -5,6 +5,58 @@ import (
 	"testing"
 )
 
+func TestTwoSumSorted(t *testing.T) {
+	examples := []struct {
+		given  []int
+		target int
+		a      int
+		b      int
+		ok     bool
+	}{
+		{[]int{5, 8, 10, 10}, 20, 10, 10, true},
+		{[]int{1, 2, 3, 4}, 6, 2, 4, true},
+		{[]int{3, 8, 5, 16}, 16, 0, 0, false},
+	}
+
+	for _, example := range examples {
+		name := fmt.Sprintf("Testing for input %v", example.given)
+		t.Run(name, func(t *testing.T) {
+			a, b, ok := TwoSumSorted(example.given, example.target)
+			if a != example.a || b != example.b || ok != example.ok {
+				t.Errorf("Fail - expected %v for %d and %d but got %v for %d and %d",
+					example.ok, example.a, example.b, ok, a, b)
+			}
+		})
+	}
+}
+
+func TestThreeSumSorted(t *testing.T) {
+	examples := []struct {
+		given  []int
+		target int
+		a      int
+		b      int
+		c      int
+		ok     bool
+	}{
+		{[]int{5, 5, 8, 10, 10}, 20, 5, 5, 10, true},
+		{[]int{1, 2, 3, 4}, 6, 1, 2, 3, true},
+		{[]int{3, 8, 5, 16}, 16, 3, 8, 5, true},
+		{[]int{3, 8, 5, 16}, 17, 0, 0, 0, false},
+	}
+
+	for _, example := range examples {
+		name := fmt.Sprintf("Testing for input %v", example.given)
+		t.Run(name, func(t *testing.T) {
+			a, b, c, ok := ThreeSumSorted(example.given, example.target)
+			if a != example.a || b != example.b || c != example.c || ok != example.ok {
+				t.Errorf("Fail - expected %v for %d, %d and %d but got %v for %d, %d and %d",
+					example.ok, example.a, example.b, example.c, ok, a, b, c)
+			}
+		})
+	}
+}
+
 func TestTwoSum(t *testing.T) {
 	examples := []struct {
 		given  []int
@@ -15,7 +67,7 @@ func TestTwoSum(t *testing.T) {
 	}{
 		{[]int{5, 8, 10, 10}, 20, 10, 10, true},
 		{[]int{1, 2, 3, 4}, 6, 2, 4, true},
-		{[]int{3, 8, 5, 16}, 16, -1, -1, false},
+		{[]int{3, 8, 5, 16}, 16, 0, 0, false},
 	}
 
 	for _, example := range examples {
@@ -41,8 +93,8 @@ func TestThreeSum(t *testing.T) {
 	}{
 		{[]int{5, 5, 8, 10, 10}, 20, 5, 5, 10, true},
 		{[]int{1, 2, 3, 4}, 6, 1, 2, 3, true},
-		{[]int{3, 8, 5, 16}, 16, 3, 8, 5, true},
-		{[]int{3, 8, 5, 16}, 17, -1, -1, -1, false},
+		{[]int{3, 8, 5, 16}, 16, 3, 5, 8, true},
+		{[]int{3, 8, 5, 16}, 17, 0, 0, 0, false},
 	}
 
 	for _, example := range examples {
@@ -52,6 +104,37 @@ func TestThreeSum(t *testing.T) {
 			if a != example.a || b != example.b || c != example.c || ok != example.ok {
 				t.Errorf("Fail - expected %v for %d, %d and %d but got %v for %d, %d and %d",
 					example.ok, example.a, example.b, example.c, ok, a, b, c)
+			}
+		})
+	}
+}
+
+func TestSubArraySum(t *testing.T) {
+	examples := []struct {
+		given  []int
+		target int
+		a      int
+		b      int
+		ok     bool
+	}{
+		{[]int{5}, 5, 0, 0, true},
+		{[]int{5, 5}, 10, 0, 1, true},
+		{[]int{5, -5}, 0, 0, 1, true},
+		{[]int{-3, 8, 5, 13}, 10, 0, 2, true},
+		{[]int{5, 8, 10, 10, 2, 5, 2, 4, 5}, 13, 0, 1, true},
+		{[]int{5, 8, 10, 10, 2, 5, 2, 4, 5}, 22, 2, 4, true},
+		{[]int{5, 8, 10, 10, 2, 5, 2, 4, 5}, 50, 0, 0, false},
+		{[]int{3, 18, 5, 13}, 16, 0, 0, false},
+		{[]int{5, 2, 5}, 10, 0, 0, false},
+	}
+
+	for _, example := range examples {
+		name := fmt.Sprintf("Testing for input %v", example.given)
+		t.Run(name, func(t *testing.T) {
+			a, b, ok := SubArraySum(example.given, example.target)
+			if a != example.a || b != example.b || ok != example.ok {
+				t.Errorf("Fail - expected %v for %d and %d but got %v for %d and %d",
+					example.ok, example.a, example.b, ok, a, b)
 			}
 		})
 	}
