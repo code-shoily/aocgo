12e7c59d97a9a52ca91f165f132d1ee733246bf8
Mafinar K <mafinar@gmail.com>
2023-01-26 01:43:51 -0500

    Update test for 18/7

--- year18/day07/main_test.go
+++ year18/day07/main_test.go
@@ -6,14 +6,14 @@ import (
 
 func TestSolve(t *testing.T) {
 	solve1, solve2 := solve(input)
-	expected1, expected2 := 0, 0
+	expected1, expected2 := "BCADPVTJFZNRWXHEKSQLUYGMIO", 0
 
 	if solve1 != expected1 {
-		t.Errorf("Fail - part 1. Expected %d, got %d", expected1, solve1)
+		t.Errorf("Fail - part 1. Expected %v, got %v", expected1, solve1)
 	}
 
 	if solve2 != expected2 {
-		t.Errorf("Fail - part 2. Expected %d, got %d", expected2, solve2)
+		t.Errorf("Fail - part 2. Expected %v, got %v", expected2, solve2)
 	}
 }
 
